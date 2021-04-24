8f08665e6b2155ed7d876b28289bbab4a2b080a0
BurakOlga <Oljaburak@mail.ru>
2021-04-25 02:47:35 +0300

    save esLint

--- js/app.js
+++ js/app.js
@@ -3,18 +3,14 @@ const todoButton = document.querySelector('#task__button');
 const todoList = document.querySelector('#list__Todo');
 const todoSearch = document.querySelector('#search');
 
-
 let tasks;
 !localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
 
 let todoElements = [];
 
+// создание структуры задачи
 
-
-//создание структуры задачи
-
-const createElements = (task, index) => {
-   return `
+const createElements = (task, index) => `
    <div class = "case ${task.completed ? 'checked' : ''} ">
       
       <li  class = " ${task.important ? 'important' : 'not--important'} flex"   ${task.completed ? 'checked' : ''}>
@@ -27,165 +23,156 @@ const createElements = (task, index) => {
       </div>
       </li>
    </div>
-   `
-}
+   `;
 
 // добавление всех задач на экран
 
 const fillHtmlList = () => {
-   todoList.innerHTML = "";
-   if (tasks.length > 0){
-      tasks.forEach((item, index) => {
-         todoList.innerHTML += createElements(item, index);
-      });
+  todoList.innerHTML = '';
+  if (tasks.length > 0) {
+    tasks.forEach((item, index) => {
+      todoList.innerHTML += createElements(item, index);
+    });
 
-      todoElements = document.querySelectorAll('.case');
-      
-   }
-}
+    todoElements = document.querySelectorAll('.case');
+  }
+};
 
 fillHtmlList();
 
 // добавление localStorage
-const updateLocal = () =>{
-   localStorage.setItem('tasks', JSON.stringify(tasks))
-}
+const updateLocal = () => {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
 
-// отмечаем выполненную задачу 
-const completeTask = index =>{
-   tasks[index].completed = !tasks[index].completed;
-   if (tasks[index].completed){
-       todoElements[index].classList.add('checked');
-   } else {
-      todoElements[index].classList.remove('checked');
-   }
-   updateLocal();
-   fillHtmlList();
-}
-
+// отмечаем выполненную задачу
+const completeTask = (index) => {
+  tasks[index].completed = !tasks[index].completed;
+  if (tasks[index].completed) {
+    todoElements[index].classList.add('checked');
+  } else {
+    todoElements[index].classList.remove('checked');
+  }
+  updateLocal();
+  fillHtmlList();
+};
 
 // удаляем задачу
 
-const deleteTask = index =>{
-   tasks.splice(index,1);
-   updateLocal();
-   fillHtmlList();
-}
+const deleteTask = (index) => {
+  tasks.splice(index, 1);
+  updateLocal();
+  fillHtmlList();
+};
 
 // важная задача
 
-const importantTask = index =>{
-   tasks[index].important = !tasks[index].important;
+const importantTask = (index) => {
+  tasks[index].important = !tasks[index].important;
 
+  updateLocal();
+  fillHtmlList();
+};
 
-   updateLocal();
-   fillHtmlList();
+// создание задачи
+function Task(description) {
+  this.description = description;
+  this.completed = false;
+  this.important = false;
 }
 
-//создание задачи
-function Task(description){
-   this.description = description;
-   this. completed = false;
-   this. important = false;
-}
-
-//добавление задачи
+// добавление задачи
 
 todoButton.addEventListener('click', (e) => {
-    if (todoInput.value === '') return;
+  if (todoInput.value === '') return;
 
-   tasks.unshift(new Task(todoInput.value));
+  tasks.unshift(new Task(todoInput.value));
 
-  
-   updateLocal();
-   fillHtmlList();
+  updateLocal();
+  fillHtmlList();
 
-   todoInput.value = '';
-
+  todoInput.value = '';
 });
 
 // добавление задачи по enter
 
-todoInput.addEventListener('keyup', function (e){
-   if (e.keyCode === 13){
-   tasks.unshift(new Task(todoInput.value));
-   updateLocal();
-   fillHtmlList();
+todoInput.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    tasks.unshift(new Task(todoInput.value));
+    updateLocal();
+    fillHtmlList();
 
-   todoInput.value = '';
-   }
-} )
+    todoInput.value = '';
+  }
+});
 
 // живой поиск
 
-todoSearch.oninput = function(){
-   let value = this.value.trim();
-   let list = document.querySelectorAll ('.case');
+todoSearch.oninput = function () {
+  const value = this.value.trim();
+  const list = document.querySelectorAll('.case');
 
-   if (value != ''){
-      list.forEach (elem => {
-         if(elem.innerText.search(value) == -1){
-            elem.classList.add('hide');
-         }
-      });
-   } 
-  else {
-      list.forEach(elem => {
-         elem.classList.remove('hide');
-      })
-   }
+  if (value != '') {
+    list.forEach((elem) => {
+      if (elem.innerText.search(value) == -1) {
+        elem.classList.add('hide');
+      }
+    });
+  } else {
+    list.forEach((elem) => {
+      elem.classList.remove('hide');
+    });
+  }
 
-   console.log (list);
-
-}
+};
 
 // переключение по вкладкам
 
-let tab = function(){
-   let tabNav = document.querySelectorAll('.components');
-      // tabContent = document.querySelectorAll('.')
-   let tabName;
+const tab = function () {
+  const tabNav = document.querySelectorAll('.components');
+  // tabContent = document.querySelectorAll('.')
+  let tabName;
 
-      tabNav.forEach(item =>{
-         item.addEventListener('click', selectTabNav)
-      });
+  tabNav.forEach((item) => {
+    item.addEventListener('click', selectTabNav);
+  });
 
-      function selectTabNav(){
-         tabNav.forEach(item =>{
-            item.classList.remove('is-active');
-         });
-         this.classList.add('is-active');
-         tabName = this.getAttribute('data-tab-name');
-        selectTabContent (tabName);
-        //console.log (tabName);
-      }
+  function selectTabNav() {
+    tabNav.forEach((item) => {
+      item.classList.remove('is-active');
+    });
+    this.classList.add('is-active');
+    tabName = this.getAttribute('data-tab-name');
+    selectTabContent(tabName);
+    // console.log (tabName);
+  }
 
-      function selectTabContent (tabName){
-         let list = document.querySelectorAll ('.case');
-         switch (tabName){
-            case 'tab-1':  
-            list.forEach(item =>{
-               item.classList.remove('tab');
-            });
-            break;
-            case 'tab-2':  
-            list.forEach(item =>{
-               item.classList.remove('tab');
-               if (item.className.indexOf("checked") !== -1){
-                  item.classList.add('tab');
-               }
-            });
-            break;
-            case 'tab-3':  
-            list.forEach(item =>{
-               item.classList.remove('tab');
-               if (item.className.indexOf("checked") == -1){
-                  item.classList.add('tab');
-               }
-            });
-            break;
-         }
-      }    
+  function selectTabContent(tabName) {
+    const list = document.querySelectorAll('.case');
+    switch (tabName) {
+      case 'tab-1':
+        list.forEach((item) => {
+          item.classList.remove('tab');
+        });
+        break;
+      case 'tab-2':
+        list.forEach((item) => {
+          item.classList.remove('tab');
+          if (item.className.indexOf('checked') !== -1) {
+            item.classList.add('tab');
+          }
+        });
+        break;
+      case 'tab-3':
+        list.forEach((item) => {
+          item.classList.remove('tab');
+          if (item.className.indexOf('checked') == -1) {
+            item.classList.add('tab');
+          }
+        });
+        break;
+    }
+  }
 };
 
 tab();
